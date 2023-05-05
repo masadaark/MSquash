3361d1f25ff81663233c48cfef40bdebc8e55c2e
Mark Masada <masada.hir@turbo.co.th>
2023-05-05 15:32:54 +0700

    domoVideo

--- server.go
+++ server.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"MSquash/controller"
+	"MSquash/service"
+
 	"github.com/gin-gonic/gin"
+)
+
+var (
+	videoService    service.VideoService       = service.New()
+	VideoController controller.VideoController = controller.New(videoService)
 )
 
 func main() {
 	server := gin.Default()
 	server.GET("/healthcheck", Healthcheck)
+	server.GET("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, VideoController.FindAll())
+	})
+	server.POST("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, VideoController.Save(ctx))
+	})
+
 	server.Run(":8080")
 }
 
