c4f7d3fa43e91a1a5404a869ec02c6bebe2fc40d
Mark Masada <masada.hir@turbo.co.th>
2023-05-05 15:54:57 +0700

    noDupplicateUrl

--- service/video-service.go
+++ service/video-service.go
@@ -1,9 +1,12 @@
 package service
 
-import "MSquash/entity"
+import (
+	"MSquash/entity"
+	"fmt"
+)
 
 type VideoService interface {
-	Save(entity.Video) entity.Video
+	Save(entity.Video) (entity.Video, error)
 	FindAll() []entity.Video
 }
 
@@ -11,15 +14,22 @@ type videoService struct {
 	videos []entity.Video
 }
 
-func New() VideoService {
-	return &videoService{}
-}
-
-func (service *videoService) Save(video entity.Video) entity.Video {
+func (service *videoService) Save(video entity.Video) (entity.Video, error) {
+	for _, v := range service.videos {
+		if v.URL == video.URL {
+			return entity.Video{}, fmt.Errorf("video with URL %s already exists", video.URL)
+		}
+	}
 	service.videos = append(service.videos, video)
-	return video
+	return video, nil
 }
 
 func (service *videoService) FindAll() []entity.Video {
 	return service.videos
+}
+
+func New() VideoService {
+	return &videoService{
+		videos: []entity.Video{},
+	}
 }
